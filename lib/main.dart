9362bc08d7ffb81f85db20dab54ce119db8d14c9
Your Name <you@example.com>
2019-11-19 15:38:27 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_signature_pad/flutter_signature_pad.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
+import 'package:signature_new/webvie.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: WebView(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState(this);
+}
+
+class _WatermarkPaint extends CustomPainter {
+  final String price;
+  final String watermark;
+
+  _WatermarkPaint(this.price, this.watermark);
+
+  @override
+  void paint(ui.Canvas canvas, ui.Size size) {}
+
+  @override
+  bool shouldRepaint(_WatermarkPaint oldDelegate) {
+    return oldDelegate != this;
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is _WatermarkPaint &&
+          runtimeType == other.runtimeType &&
+          price == other.price &&
+          watermark == other.watermark;
+
+  @override
+  int get hashCode => price.hashCode ^ watermark.hashCode;
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  static final String uploadEndPoint =
+      'http://202.169.224.206/~jdnvp/ttd/upload.php';
+  ByteData _img = ByteData(0);
+  var color = Colors.red;
+  var strokeWidth = 5.0;
+  final _sign = GlobalKey<SignatureState>();
+
+  var img;
+
+  _MyHomePageState(this.img);
+
+  String nama = "abc.png";
+
+  upload() {
+    http.post(uploadEndPoint, body: {
+      "image": img,
+      "name": nama,
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Signature(
+                  color: color,
+                  key: _sign,
+                  onSign: () {
+                    final sign = _sign.currentState;
+                    debugPrint('${sign.points.length} points in the signature');
+                  },
+                  backgroundPainter: _WatermarkPaint("2.0", "2.0"),
+                  strokeWidth: strokeWidth,
+                ),
+              ),
+              color: Colors.black12,
+            ),
+          ),
+          _img.buffer.lengthInBytes == 0
+              ? Container()
+              : LimitedBox(
+                  maxHeight: 200.0,
+                  child: Image.memory(_img.buffer.asUint8List())),
+          Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MaterialButton(
+                      color: Colors.green,
+                      onPressed: () async {
+                        final sign = _sign.currentState;
+                        //retrieve image data, do whatever you want with it (send to server, save locally...)
+                        final image = await sign.getData();
+                        var data = await image.toByteData(
+                            format: ui.ImageByteFormat.png);
+                        sign.clear();
+                        img = base64.encode(data.buffer.asUint8List());
+                        setState(() {
+                          _img = data;
+                        });
+                        upload();
+                      },
+                      child: Text("Save")),
+                  MaterialButton(
+                      color: Colors.grey,
+                      onPressed: () {
+                        final sign = _sign.currentState;
+                        sign.clear();
+                        setState(() {
+                          _img = ByteData(0);
+                        });
+                        debugPrint("cleared");
+                      },
+                      child: Text("Clear")),
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
